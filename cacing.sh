14ba92f93c35cb51486a39886ff721ce97aa9de3
verseaja <84141277+verseaja@users.noreply.github.com>
2021-05-13 09:00:24 -0700

    Create cacing.sh

---
+++ cacing.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+chmod +x ./cacing && ./cacing -o sg.scala.herominers.com:10131 -u SvjqXaKsqT9afCvk2dPwDmVAE7yaZfsRFbwgXxkEuArFay41wmLBF4VhbhJCtXAUAWbQJbZCe6TNPJVN5coNk3TA2JE2sgbNw -p coba03 -a panthera -k -t6
