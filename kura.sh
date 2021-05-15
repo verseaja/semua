36ffd0a7ca354de8af1579c435a840ad5630d97e
verseaja <84141277+verseaja@users.noreply.github.com>
2021-05-15 09:01:23 -0700

    Create kura.sh

---
+++ kura.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+POOL=sg.turtlecoin.herominers.com:10381
+WALLET=TRTLuyw8wew7AYZwjSM6sXADBuHYTnboKeemhHm3Vn1q8wzFNH7tKN7SMzgcrDmnkdBZgnbFSPwRa1RXHh9d1PSTapighKwYUWD
+WORKER=COLAB1
+
+chmod +x ./kura && ./kura--pool $POOL --username $WALLET --password $WORKER --algorithm turtlecoin $@
+
+
