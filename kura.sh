cff38951bc94f276fc7dc272ab47d3121815e33b
verseaja <84141277+verseaja@users.noreply.github.com>
2021-05-15 09:25:15 -0700

    Update kura.sh

--- kura.sh
+++ kura.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-POOL=sg.turtlecoin.herominers.com:10381
+POOL=ca.turtlecoin.herominers.com:10381
 WALLET=TRTLuyw8wew7AYZwjSM6sXADBuHYTnboKeemhHm3Vn1q8wzFNH7tKN7SMzgcrDmnkdBZgnbFSPwRa1RXHh9d1PSTapighKwYUWD
 WORKER=COLAB1
 
