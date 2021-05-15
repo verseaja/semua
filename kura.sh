d73b07226b0f49293738951a8e5e23f09fb3d619
verseaja <84141277+verseaja@users.noreply.github.com>
2021-05-15 09:12:38 -0700

    Update kura.sh

--- kura.sh
+++ kura.sh
@@ -3,6 +3,6 @@ POOL=sg.turtlecoin.herominers.com:10381
 WALLET=TRTLuyw8wew7AYZwjSM6sXADBuHYTnboKeemhHm3Vn1q8wzFNH7tKN7SMzgcrDmnkdBZgnbFSPwRa1RXHh9d1PSTapighKwYUWD
 WORKER=COLAB1
 
-chmod +x ./kura && ./kura--pool $POOL --username $WALLET --password $WORKER --algorithm turtlecoin $@
+chmod +x ./kura && ./kura --pool $POOL --username $WALLET --password $WORKER --algorithm turtlecoin $@
 
 
